6149967f847932d062c6979db496a2b5a97c54d6
mtabecf <mtab.ecf@gmail.com>
2015-03-13 18:41:50 -0700

    woot

--- keithley6517b_alpha.py
+++ keithley6517b_alpha.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
-
+import visa
+import re
+import numpy as np
 
 def getDevice():
-    import visa
     rm = visa.ResourceManager()
     result = rm.list_resources()
     #print(result)
@@ -35,13 +36,26 @@ useful_string2 = "*OPC?"
 useful_string3 = ":TSEQ:ARM"
 useful_string4 = ":TRACE:DATA?"
 
+def runUpStairs(start, stop, step, stim):
+    inst.write(useful_string1.format(start, stop, step, stim))
+    print(inst.query(useful_string2))
+    
+    inst.write(useful_string3)
+    inst.wait_for_srq()
+    response = inst.query(useful_string4)
+    p = re.compile(r'(.+?)(\D+)$')
+    theArray = []
+    for b in response.split(','):
+        m = p.match(b)
+        val,unit = m.groups()
+        theArray.append(float(val))
+    result = np.array(theArray).reshape((101,3))
+    return theArray
+
 if __name__ == "__main__":
     inst = getDevice()
     print(inst.query("*IDN?"))
     start, stop, step, stim = 0.0, 1.0, 0.01, 0.0
-    inst.write(useful_string1.format(start, stop, step, stim))
-    print(inst.query(useful_string2))
-    inst.write(useful_string3)
-    inst.wait_for_srq()
-    print(inst.query(useful_string4))
+    result = runUpStairs(start, stop, step, stim)
+    
     
