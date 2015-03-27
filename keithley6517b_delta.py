d5692847dec3cc72a8be2df1c4782a98030825f8
mtabecf <mtab.ecf@gmail.com>
2015-03-26 18:18:26 -0700

    electrons

--- keithley6517b_delta.py
+++ keithley6517b_delta.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8
 """
 This will the IV curve tracer cum open cicruit mode.
+To learn how to do, look for Reference manual '6517b-901-01--B-Jun 2009--Ref.pdf'.
+Chapter 14 contains SCPI command reference.
+
+Documentation for PyVISA is online.
 """
 
 from __future__ import print_function
@@ -12,6 +16,7 @@ from collections import namedtuple
 import pickle
 import datetime
 import siteDefs
+import time
 
 def getDevice():
     rm = visa.ResourceManager()
@@ -29,6 +34,8 @@ def getDevice():
 useful_string0 = ":FORM:ELEM READ,RNUM,UNIT,TST,STAT,VSO"
 # Example output looks like this:
 example_read = "+01.01103E+00NVDC,09:00:20.00,26-Mar-2015,+01945RDNG#,+0001.000Vsrc"
+# However, when running a sequence mode, the VSO is ommitted, like:
+example_read_seq = "+00.00849E+00NVDC,09:43:20.00,26-Mar-2015,+00000RDNG#"
 
 useful_string1 = """*rst;*CLS;
 stat:meas:enab 512;
@@ -67,12 +74,15 @@ def runUpStairs(inst, start, stop, step, stim, shape):
     inst.wait_for_srq()
     response = inst.query(useful_string4)
     print(response)
-    p = re.compile(r'(.+?)(\D+)$')
+    p = re.compile(r'(.+?)(\D*)$')
     theArray = []
     unitArray = []
     for b in response.split(','):
+        print(b)
         m = p.match(b)
         val,unit = m.groups()
+        #if val.contains(':'):
+        #    theArray.append(datetime.fromstring(val)
         theArray.append(float(val))
         unitArray.append(unit)
     result = np.array(theArray).reshape(shape)
@@ -92,8 +102,22 @@ def sourceVoltageReadVoltage(inst,Vin):
 def main(date, outputPickle, outputPlot):
     rm,inst = getDevice()
     print(inst.query("*IDN?"))
+    inst.write(':DISP:TEXT:DATA "Clinton 2016"')
+    inst.write(':DISP:TEXT:STAT 1')
+    inst.write(':DISP:WIND2:TEXT:DATA "Loading   ..."')
+    inst.write(':DISP:WIND2:TEXT:STAT 1')
+    time.sleep(1)
+    inst.write(':DISP:WIND2:TEXT:DATA "Loading   ... platform"')
+    time.sleep(1)
+    inst.write(':DISP:WIND2:TEXT:DATA "Loading   ... electrons"')
+    time.sleep(1)
+    inst.write(':DISP:WIND2:TEXT:DATA "electrons ... electrons"')
+    time.sleep(1)
+    inst.write(':DISP:TEXT:STAT 0')
+    inst.write(':DISP:WIND2:TEXT:STAT 0')
+    
     inst.write(useful_string0)
-    start, stop, step, stim = -0.5, 0.5, 0.01, 0.0
+    start, stop, step, stim = -0.0, 0.5, 0.01, 0.0
     npoints = 1+(stop-start)/step
     nfields = 3
     shape = npoints,nfields
@@ -103,7 +127,7 @@ def main(date, outputPickle, outputPlot):
     #Vin = np.arange(start,stop,step)
     Vin = np.linspace(start,stop,npoints)
     #print(result)
-    Vout, time, iSample = result.T
+    Vout, timestamp, iSample = result.T
     R_current_sensor = 98.3 # ohms
     V_current_sensor = Vin + Vout
     I = V_current_sensor / R_current_sensor
