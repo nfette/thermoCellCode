8c6d1d8cd845c78c03d9e20adc8460bcc720b9f9
nfette <nfette@gmail.com>
2015-04-02 16:53:29 -0700

    Coffee restores ability to work

--- libkeithley6517b.py
+++ libkeithley6517b.py
@@ -9,9 +9,74 @@ Chapter 14 contains SCPI command reference.
 
 Documentation for PyVISA is online.
 """
+from __future__ import print_function
+import visa
 from collections import namedtuple
 import re
 import datetime
+import pickle
+
+"""
+    Opening and configuring the device.
+"""
+def getDevice():
+    rm = visa.ResourceManager()
+    result = rm.list_resources()
+    #print(result)
+    for name in result:
+        if name.find("GPIB") >= 0:
+            myGPIBname = name
+            break
+    inst = rm.open_resource(myGPIBname)
+    #print(inst.query("*IDN?"))
+    return rm,inst
+
+def configVSO(inst):
+    inst.write("""*rst;*CLS;
+stat:meas:enab 512;
+:SENS:FUNC 'VOLT';
+:SENS:CURR:RANG:AUTO ON;
+""")
+    inst.write(defaultFormat.getFormatCommand())
+    
+def sourceVoltageReadVoltage(inst,Vin,stayOn=False):
+    inst.write(":SOUR:VOLT {}".format(Vin))
+    try:
+        inst.write(":OUTP {:b}".format(True))
+        response = inst.query(":READ?")
+        inst.write("OUTP {:b}".format(stayOn))
+        return parseRead(defaultFormat, response)
+    finally:
+        # There was an error. Turn off the voltage source.
+        inst.write("OUTP {:b}".format(False))
+
+def configStairSweep(inst):
+    inst.write("""*rst;*CLS;
+stat:meas:enab 512;
+*sre 1;
+:SENS:FUNC 'VOLT';
+:SENS:CURR:RANG:AUTO ON;
+""")
+    inst.write(seqFormat.getFormatCommand())
+
+def runUpStairs(inst, start, stop, step, stim):
+    inst.write(""":TSEQ:TYPE STSW;
+:TSEQ:STSW:STAR {};
+:TSEQ:STSW:STOP {};
+:TSEQ:STSW:STEP {};
+:TSEQ:STSW:STIM {};
+:TSEQ:TSO imm;
+""".format(start, stop, step, stim))
+    print(inst.query("*OPC?"))
+    
+    inst.write(":TSEQ:ARM")
+    inst.wait_for_srq()
+    response = inst.query("TRACE:DATA?")
+    return parseTraceData(seqFormat, response)
+
+"""
+    Read and parse data!
+"""
 
 formatFlags = namedtuple('FormatFlags',
                          ['READ','CHAN','RNUM','UNIT','TST',
@@ -95,15 +160,22 @@ def parseTraceData(format_flags, s):
 def unitTestParseRead():
     # See the :FORMat:ELEMents command, page 14-36
     # Example output looks like this:
-    tx_format = ":FORM:ELEM READ,RNUM,UNIT,TST,STAT,VSO"
     rx_read = \
         "+01.01103E+00NVDC,09:00:20.00,26-Mar-2015,+01945RDNG#,+0001.000Vsrc"
-    print parseRead(defaultFormat,rx_read)
+    print("Imagine: tx> {}".format(defaultFormat.getFormatCommand()))
+    print("Imagine: tx> :READ?")
+    print("Imagine: rx< {}".format(rx_read))
+    print(parseRead(defaultFormat,rx_read))
+    print()
     
 def unitTestParseSeqData():
     # However, when running a sequence mode, the VSO is ommitted, like:
     example_read_seq = "+00.00849E+00NVDC,09:43:20.00,26-Mar-2015,+00000RDNG#"
-    print parseTraceData(seqFormat, ",".join([example_read_seq,] * 4))
+    print("Imagine: tx> {}".format(seqFormat.getFormatCommand()))
+    print("Imagine: tx> :TRACE:DATA?")
+    print("Imagine: rx< {}".format(example_read_seq))
+    print(parseTraceData(seqFormat, ",".join([example_read_seq,] * 4)))
+    print()
 
 def main():
     unitTestParseRead()
