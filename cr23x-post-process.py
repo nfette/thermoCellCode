e3b75f23bb45673e3f80fdefb823cc271fac6208
nfette <nfette@gmail.com>
2015-09-05 05:14:16 -0700

    Post process temperature data
    Quickly enough

--- cr23x-post-process.py
+++ cr23x-post-process.py
@@ -4,7 +4,9 @@ import string
 import os
 import libcr23x
 import siteDefs
-
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import sys
 
 if len(sys.argv) == 2:
@@ -13,8 +15,67 @@ else:
     myfile = "cr23x_outputs_2015-08-30T20=00=45.371000.dat"
     myfile = "{}{}".format(siteDefs.data_base_dir, myfile)
 
-with open(myfile, 'r') as f:
-    for line in f:
-        print line
+basename = os.path.basename(myfile)
+startdate = datetime.datetime.strptime(basename,
+                                       "cr23x_outputs_%Y-%m-%dT%H=%M=%S.%f.dat")
 
-    
+with open(myfile, 'r') as f:
+    line1 = f.readline()
+    line2 = f.readline()
+
+cols = len(line1.split(','))
+data = np.genfromtxt(myfile, delimiter=",",names=True, usecols=range(1,cols),
+                     skip_footer=2)
+
+# Note that the CR23x timestamp does not include date. So attach that...
+# because otherwise, if the data run through midnight, then they are disordered.
+d = startdate.date()
+def fun1(s):
+    try:
+        t = datetime.datetime.strptime(s,"%H:%M:%S.%f").time()
+    except:
+        t = datetime.datetime.strptime(s,"%H:%M:%S").time()
+    newtime = datetime.datetime.combine(d,t)
+    return newtime
+
+dataT = np.genfromtxt(myfile, delimiter=",",names=True,
+                      usecols=[0], dtype='datetime64', converters={0:fun1},
+                      skip_footer=2)
+a = np.nonzero(np.diff(dataT) < datetime.timedelta(minutes=-1))[0]
+for i in a:
+    dataT[1+i:] += datetime.timedelta(1)
+
+# Some data processing
+Th = 0.5 * data['T_h_dry'] + 0.5 * data['T_h_wet']
+Tc = 0.5 * data['T_c_dry'] + 0.5 * data['T_c_wet']
+plt.plot(Tc, Th, 'o')
+plt.xlabel('Cold electrode, $^\circ C$')
+plt.ylabel('Hot electrode, $^\circ C$')
+plt.title(basename)
+plt.savefig(myfile + ".fig1.png")
+plt.close()
+
+Tave = 0.5 * Th + 0.5 * Tc
+Tdelta = Th - Tc
+plt.plot(Tave, Tdelta, 'o')
+plt.xlabel('Average temperature, $^\circ C$')
+plt.ylabel('Temperature difference, $^\circ C$')
+plt.title(basename)
+plt.savefig(myfile + ".fig2.png")
+plt.close()
+
+plt.figure(figsize=(16,8))
+#plt.xticks(rotation=20)
+for name in data.dtype.names:    
+    plt.plot(dataT, data[name], label=name)
+plt.setp( plt.gca().xaxis.get_majorticklabels(), rotation=20, horizontalalignment='right' )
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+plt.gcf().autofmt_xdate()
+plt.gca().grid(True,axis='x')
+plt.legend(loc='best')
+plt.xlabel('Time')
+plt.ylabel('Temperature $^\circ C$')
+plt.ylim([0,130])
+plt.title(basename)
+plt.savefig(myfile + ".fig3.png")
+plt.close()
